54b58b905b3422e029edb47ac91ea80287d43255
RedHood <alanohcz94@gmail.com>
2022-09-12 22:36:52 +0800

    Update code

--- UdemyWebDev/socialCircleApp/client/src/App.js
+++ UdemyWebDev/socialCircleApp/client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+          <Navbar />
+          <section className="container">
+          <Routes>
+            <Route path='/' element={<Landing/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+          </Routes> 
+          </section>  
+      </Fragment>
+    </Router>
   );
 }
 
